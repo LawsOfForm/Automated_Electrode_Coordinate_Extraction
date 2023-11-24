06fe3280f7bd11ad0339b037558cc24de42c3b4e
sriemannn <66158608+sriemannn@users.noreply.github.com>
2023-11-24 08:03:58 +0100

    add a cylindrical roi and tilt it, so it matches the head

--- cylinder_roi/create_cylinder.py
+++ cylinder_roi/create_cylinder.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy import ndimage
 import nibabel as nib
 
 def circle_mask(xdim, ydim, radius, centre):
@@ -35,14 +34,42 @@ def get_transformation_matrix(a, b):
     
     return R
 
-def pad_tranformation_matrix(R):
+def create_transformation_matrix(v1, v2):
+    # Normalisieren Sie die Vektoren
+    v1 = v1 / np.linalg.norm(v1)
+    v2 = v2 / np.linalg.norm(v2)
+
+    # Berechnen Sie die Rotationsachse und den Winkel
+    axis = np.cross(v1, v2)
+    angle = np.arccos(np.dot(v1, v2))
+
+    # Erstellen Sie die Rotationsmatrix
+    rotation = R.from_rotvec(axis * angle)
+    return rotation.as_dcm()
+
+def pad_transformation_matrix(R):
     R_pad = np.eye(4)
     R_pad[:3, :3] = R
     return R_pad
+
+def rotate_img_obj(img, R, centre):
+    centre = centre.reshape(3, 1)
+    cylinder_ind = np.where(img == 1)
+    centered_cylinder_ind = cylinder_ind - centre
+    rotated_centered_cylinder_ind = R @ centered_cylinder_ind
+    rotated_cylinder_ind = (
+        rotated_centered_cylinder_ind + centre).T
+    rotated_cylinder_ind = np.vstack((
+        np.floor(rotated_cylinder_ind),
+        np.ceil(rotated_cylinder_ind)
+    )).astype("int32")
+    
+    return np.unique(rotated_cylinder_ind, axis = 0)
+
     
 if __name__ == "__main__":
     nifti = nib.load("/media/MeMoSLAP_Subjects/derivatives/automated_electrode_extraction/sub-010/electrode_extraction/ses-1/run-01/finalmask.nii.gz")
-
+    sub_dir = "/media/MeMoSLAP_Subjects/derivatives/automated_electrode_extraction/sub-010/electrode_extraction/ses-1/run-01/"
     centre = np.array([176, 165, 227])
     normal = np.array([89, 21, 70])
 
@@ -61,15 +88,23 @@ if __name__ == "__main__":
     mask_img = empty_img
     
     R = get_transformation_matrix(np.array([0, 0, 1]), normal)
-    R_pad = pad_tranformation_matrix(R)
+    R_pad = pad_transformation_matrix(R)
     
+    rotated_cylinder_ind = rotate_img_obj(mask_img, R, centre)
+    
+    emtpy_img = np.zeros((nifti.shape))
+    
+    emtpy_img[
+        rotated_cylinder_ind[:, 0],
+        rotated_cylinder_ind[:, 1],
+        rotated_cylinder_ind[:, 2]
+    ] = 1
+     
     new_img = nib.Nifti1Image(
-        mask_img,
+        empty_img,
         nifti.affine,
         nifti.header
     )
     
     nib.save(new_img,
         "/media/MeMoSLAP_Subjects/derivatives/automated_electrode_extraction/sub-010/electrode_extraction/ses-1/run-01/cylinder_test.nii.gz")
-
-    
