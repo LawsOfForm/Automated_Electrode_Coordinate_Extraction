b0e6e0f9ccc208d721b6b814c4d99d16822ef977
sriemannn <66158608+sriemannn@users.noreply.github.com>
2023-11-27 16:21:06 +0100

    better warnings, process monitoring

--- cylinder_roi/refine_segmentation.py
+++ cylinder_roi/refine_segmentation.py
@@ -1,7 +1,10 @@
+import logging
 import os.path as op
+import re
 
 import nibabel as nib
 import numpy as np
+from alive_progress import alive_it
 from paths import glob_sub_dir, root_dir
 from skimage.feature import canny
 from skimage.morphology import convex_hull_image
@@ -33,11 +36,28 @@ if __name__ == "__main__":
 
     sub_dirs = glob_sub_dir(root_dir)
 
-    for sub_dir in sub_dirs:
+    if sub_dirs is None:
+        raise FileNotFoundError(
+            f"No files found in {root_dir} matching the folder structure"
+        )
+
+    for sub_dir in alive_it(sub_dirs):
+        sub, ses, run = re.findall(sub_dir, r"([0-9]+)")
         petra_path = op.join(sub_dir, "petra_.nii.gz")
         cylinder_mask_path = op.join(sub_dir, "cylinder_test.nii.gz")
 
-        if not op.exists(petra_path) or not op.exists(cylinder_mask_path):
+        if not op.exists(petra_path):
+            logging.warning(
+                f"Did not find {petra_path} for sub-{sub}, ses-{ses}, run-{run}.\n"
+                + "Will skip the subject"
+            )
+            continue
+
+        if not op.exists(cylinder_mask_path):
+            logging.warning(
+                f"Did not find {cylinder_mask_path} for sub-{sub}, ses-{ses}, run-{run}.\n"
+                + "Will skip the subject"
+            )
             continue
 
         petra, petra_img = load_nifti(petra_path)
