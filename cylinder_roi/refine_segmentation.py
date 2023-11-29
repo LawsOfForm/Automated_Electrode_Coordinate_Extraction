e7850102487f5491e6293ec90afb54a5a4e2c9ed
sriemannn <steffenriemann@web.de>
2023-11-29 08:38:53 +0100

    added finalmask and layers as substraction

--- cylinder_roi/refine_segmentation.py
+++ cylinder_roi/refine_segmentation.py
@@ -8,9 +8,17 @@ import numpy as np
 from alive_progress import alive_it
 from paths import glob_sub_dir, root_dir
 from skimage.feature import canny
-from skimage.morphology import convex_hull_image, dilation, square
+from skimage.morphology import (
+    convex_hull_image,
+    dilation,
+    disk,
+    square,
+    white_tophat,
+)
 from util.io import load_nifti, save_nifti
+from util.roi import binarize_img
 
+root_dir = "/home/steffen"
 sub_dirs = glob_sub_dir(root_dir)
 if sub_dirs is None:
     raise FileNotFoundError(
@@ -18,6 +26,7 @@ if sub_dirs is None:
     )
 
 for sub_dir in alive_it(sub_dirs):
+    logging.basicConfig(level=logging.INFO)
     sub, ses, run = re.findall(r"([0-9]+)", sub_dir)
     petra_path = op.join(sub_dir, "petra_.nii.gz")
     cylinder_mask_path = op.join(sub_dir, "cylinder_ROI.nii.gz")
@@ -38,21 +47,36 @@ for sub_dir in alive_it(sub_dirs):
 
     petra, petra_img = load_nifti(petra_path)
     _, mask_img = load_nifti(cylinder_mask_path)
+    _, layers_img = load_nifti(op.join(sub_dir, "layers.nii.gz"))
+    _, head_mask_img = load_nifti(op.join(sub_dir, "finalmask.nii.gz"))
+
+    logging.info(f"Number of Voxel pre-dilation: {np.sum(mask_img)}")
 
     mask_img_dil = np.array(
         [
-            dilation(mask, square(15)) if np.any(mask) else mask
+            dilation(mask, square(3)) if np.any(mask) else mask
             for mask in mask_img
         ]
+    )
+    logging.info(f"Number of Voxel post-dilation: {np.sum(mask_img_dil)}")
+
+    layers_img_bin = binarize_img(layers_img, threshold=3)
+    head_mask = binarize_img(head_mask_img, threshold=1)
+    mask_img_dil_cut = mask_img_dil * ~layers_img_bin * ~head_mask
+    masked_petra = petra_img * mask_img_dil_cut
+
+    logging.info(
+        f"N voxels cut from mask: {np.sum(mask_img_dil - mask_img_dil_cut)}"
+    )
+
+    masked_denoised_petra = np.array(
+        [white_tophat(img, disk(3)) for img in masked_petra]
     )
 
     sigma = 1
 
     edges = np.array(
-        [
-            canny(img, mask=mask, sigma=sigma)
-            for img, mask in zip(petra_img, mask_img_dil)
-        ]
+        [canny(img, sigma=sigma) for img in masked_denoised_petra]
     )
 
     if not np.any(edges):
