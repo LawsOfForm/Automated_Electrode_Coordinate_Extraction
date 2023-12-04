a5f52287887d44a5c849a257b874c76b923709fc
sriemannn <66158608+sriemannn@users.noreply.github.com>
2023-12-04 16:46:39 +0100

    get high intensity electrode values based on SimpleITK

---
+++ cylinder_roi/SimpleITK_refinement.py
@@ -0,0 +1,94 @@
+import logging
+import os.path as op
+import re
+
+import SimpleITK as sitk
+from alive_progress import alive_it
+from paths import glob_sub_dir, root_dir
+
+
+def mask_image(image, mask):
+    """
+    Mask image with mask
+
+    Parameters
+    ----------
+    image:
+        SimpleITK image
+    mask:
+        SimpleITK image
+
+    Returns
+    -------
+    masked image
+    """
+    # Apply mask to image
+    masker = sitk.MaskImageFilter()
+    masker.SetMaskingValue(0)
+    masker.SetOutsideValue(0)
+    masked_image = masker.Execute(image, mask)
+
+    subtracter = sitk.SubtractImageFilter()
+    masked_image = subtracter.Execute(image, masked_image)
+
+    # Return masked image
+    return masked_image
+
+
+def huang_threshold(image):
+    """
+    Huang thresholding
+
+    Parameters
+    ----------
+    image:
+        SimpleITK image
+
+    Returns
+    -------
+    thresholded image
+    """
+    # Apply Huang thresholding
+    huang_filter = sitk.HuangThresholdImageFilter()
+    huang_filter.SetInsideValue(0)
+    huang_filter.SetOutsideValue(1)
+    thresholded_image = huang_filter.Execute(image)
+
+    # Return thresholded image
+    return thresholded_image
+
+
+if __name__ == "__main__":
+    sub_dirs = glob_sub_dir(root_dir)
+
+    if sub_dirs is None:
+        raise FileNotFoundError("No sub-directories found.")
+
+    for sub_dir in alive_it(sub_dirs):
+        # logging.BasicConfig(level=logging.INFO)
+        cylinder_mask_path = op.join(sub_dir, "cylinder_ROI.nii.gz")
+        # cylinder_mask_path = op.join(sub_dir, "cylinder_plus_plug_ROI.nii.gz")
+        masked_petra = op.join(sub_dir, "petra_masked.nii.gz")
+        refined_mask = op.join(sub_dir, "refined_electrode_ROI.nii.gz")
+
+        sub, ses, run = re.findall(r"([0-9]+)", sub_dir)
+
+        if not op.exists(masked_petra):
+            continue
+        if not op.exists(cylinder_mask_path):
+            continue
+        if op.exists(refined_mask):
+            continue
+
+        logging.info(
+            f"Refining cylinder ROI for sub-{sub}, ses-{ses}, run-{run}"
+        )
+
+        petra = sitk.ReadImage(masked_petra)
+        mask = sitk.ReadImage(cylinder_mask_path)
+
+        masked_petra = mask_image(petra, mask)
+
+        thresholded_petra = huang_threshold(masked_petra)
+
+        sitk.WriteImage(thresholded_petra, cylinder_mask_path)
