1510d35d46b72c839b8146ad7d8e244b3323c68e
sriemannn <steffenriemann@web.de>
2023-12-18 16:43:07 +0100

    overlay with lower alpha for better assessment

--- cylinder_roi/report.py
+++ cylinder_roi/report.py
@@ -4,11 +4,31 @@ import re
 from glob import glob
 
 import matplotlib.pyplot as plt
+import numpy as np
 from alive_progress import alive_it
 from paths import root_dir
 from paths_funcs import glob_sub_dir
 from scipy.ndimage import center_of_mass
 from util.io import load_nifti
+
+
+def slice_at(img, coords):
+    return [
+        img[int(coords[0]), :, :],
+        img[:, int(coords[1]), :],
+        img[:, :, int(coords[2])],
+    ]
+
+
+def normalize_img(img, clip=True, rng=(10, 99)):
+    if clip:
+        p95 = np.percentile(img, rng[1])
+        p10 = np.percentile(img, rng[0])
+        img = np.clip(img, p10, p95)
+
+    img = ((img - img.min()) / (img.max() - img.min())) * 255
+    return img.astype(np.uint8)
+
 
 sub_dirs = glob_sub_dir(root_dir)
 
@@ -26,34 +46,32 @@ for sub_dir in alive_it(sub_dirs):
 
     report = op.join(report_dir, f"{sub}_{ses}_{run}.png")
 
-    if op.isfile(report):
-        continue
-
+    # if op.isfile(report):
+    #     continue
+    #
     masks = glob(op.join(sub_dir, "mask_*.nii.gz"))
 
     if not masks:
         continue
 
     petra_nii, petra = load_nifti(op.join(sub_dir, "petra_.nii.gz"))
-    petra_max = petra.max()
+    normalized_petra = normalize_img(petra, rng=(10, 99))
 
-    _, ax = plt.subplots(len(masks), 3, figsize=(12, 12))
+    _, axs = plt.subplots(len(masks), 3, figsize=(12, 12))
     for i, mask in enumerate(masks):
         mask_nii, mask = load_nifti(mask)
 
         com = center_of_mass(mask)
 
-        mask_overlayed_petra = petra.copy()
-        mask_overlayed_petra[mask == 1] = petra_max
+        petra_slices = slice_at(normalized_petra, com)
 
-        slice_0 = mask_overlayed_petra[int(com[0]), :, :]
-        slice_1 = mask_overlayed_petra[:, int(com[1]), :]
-        slice_2 = mask_overlayed_petra[:, :, int(com[2])]
+        mask_slices = slice_at(mask, com)
 
-        slices = [slice_0, slice_1, slice_2]
-
-        for j, slice_ in enumerate(slices):
-            ax[i][j].imshow(slice_, cmap="gray")
+        for j, (petra_slice, mask_slice) in enumerate(
+            zip(petra_slices, mask_slices)
+        ):
+            axs[i][j].imshow(petra_slice, cmap="gray")
+            axs[i][j].imshow(mask_slice.squeeze(), cmap="Reds", alpha=0.3)
             plt.axis("off")
 
     plt.savefig(report)
