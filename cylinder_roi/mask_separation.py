073393292652537157e843f5bfcfd25307af4f68
sriemannn <66158608+sriemannn@users.noreply.github.com>
2024-02-06 09:41:31 +0100

    added warning for too cloze electrodes

--- cylinder_roi/mask_separation.py
+++ cylinder_roi/mask_separation.py
@@ -2,6 +2,7 @@ import os.path as op
 import re
 from glob import glob
 
+import logging
 import numpy as np
 from alive_progress import alive_it
 from paths import root_dir
@@ -26,16 +27,27 @@ for sub_dir in alive_it(sub_dirs):
     if not op.exists(cylinder_masks_path):
         continue
 
-    # if glob(op.join(sub_dir, "*mask_*.nii.gz")):
-    #     continue
+    if glob(op.join(sub_dir, "*mask_*.nii.gz")):
+        continue
 
     sub, ses, run = re.findall(r"(sub-[0-9]+|ses-[0-9]+|run-[0-9]+)", sub_dir)
+
+    # if sub == "sub-016" and ses == "ses-1" and run == "run-01":
+    #     continue
+
     print(sub, ses, run)
     mask_nifti, mask = load_nifti(cylinder_masks_path)
+    mask_shape = mask.shape
 
     masks = fast_divide_mask(
         mask
     )  # BUG: sometimes returnes a list of lists, fix with recursive_flatten
+    if masks == mask.shape:
+        logging.warning(
+            "ROIs in mask %s are too close to be separated", sub_dir
+        )
+        continue
+
     masks = recursive_flatten(masks)
 
     for i, m in enumerate(masks):
