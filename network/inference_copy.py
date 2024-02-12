19adaae23645b5e60611c7fe3a3a5f5acf508052
LawsOfForm <jhwh2.0@gmail.com>
2024-02-12 10:19:13 +0100

    Add files via upload
    inference loop for all subjects

--- network/inference_copy.py
+++ network/inference_copy.py
@@ -3,8 +3,6 @@ import numpy as np
 import torch
 from tqdm import tqdm
 import os.path as op
-from paths import root_dir
-import glob
 
 
 def load_nifti(path):
@@ -28,7 +26,7 @@ def remove_color_channel(img_tensor):
     return torch.squeeze(img_tensor, dim=1)
 
 
-def main(sub_dir):
+def main():
     # device = "cuda" if torch.cuda.is_available() else "cpu"
     device = "cpu"
     unet = torch.hub.load(
@@ -48,50 +46,45 @@ def main(sub_dir):
     # nifti, img = load_nifti(
     #     "/media/MeMoSLAP_Subjects/SUBJECTS_XNAT/sub-4012/ses-1/anat/sub-4012_ses-1_acq-petra_run-01_PDw.nii.gz"
     # )  # TODO: path as input
-    base_dir = root_dir
+    base_dir = "/media/MeMoSLAP_Subjects/derivatives/automated_electrode_extraction/"
     
-    sub_dirs = glob.glob(f'{root_dir}/**/unzipped/r*PDw.nii')
+    nifti_path = op.join(
+        base_dir,
+        "sub-004",
+        "unzipped",
+        "rsub-004_ses-1_acq-petra_run-01_PDw.nii",
+    )
 
-    for sub_dir in sub_dirs:
+    nifti, img = load_nifti(op.join(nifti_path))
 
-        nifti_path = op.join(sub_dir
-            
-        )
+    img_tensor = niimg2tensor(img)
 
-        nifti, img = load_nifti(op.join(nifti_path))
+    img_tensor = add_rgb(img_tensor)
 
-        img_tensor = niimg2tensor(img)
+    img_inf = torch.zeros(img.shape)
+    img_inf = torch.unsqueeze(img_inf, dim=1)
 
-        img_tensor = add_rgb(img_tensor)
+    with torch.set_grad_enabled(False):
+        for i, x in tqdm(enumerate(img_tensor), total=len(img_tensor)):
+            x = torch.unsqueeze(x, dim=0)
+            x = x.to(device, dtype=torch.float32)
+            y_inf = unet(x)
 
-        img_inf = torch.zeros(img.shape)
-        img_inf = torch.unsqueeze(img_inf, dim=1)
+            img_inf[i] = y_inf.detach().cpu()
 
-        with torch.set_grad_enabled(False):
-            for i, x in tqdm(enumerate(img_tensor), total=len(img_tensor)):
-                x = torch.unsqueeze(x, dim=0)
-                x = x.to(device, dtype=torch.float32)
-                y_inf = unet(x)
+    img_inf = remove_color_channel(img_inf)
+    img_inf = img_inf.numpy()
+    img_inf = np.round(img_inf)
 
-                img_inf[i] = y_inf.detach().cpu()
+    inf_nifti = nib.nifti1.Nifti1Image(
+        img_inf,
+        nifti.affine,
+        nifti.header,
+    )
 
-        img_inf = remove_color_channel(img_inf)
-        img_inf = img_inf.numpy()
-        img_inf = np.round(img_inf)
+    outname = op.splitext(nifti_path)[0] + "_inference.nii.gz"
 
-        inf_nifti = nib.nifti1.Nifti1Image(
-            img_inf,
-            nifti.affine,
-            nifti.header,
-        )
-
-        outname = op.splitext(nifti_path)[0] + "_inference.nii.gz"
-    
-        nib.save(inf_nifti, outname)  # TODO: sensible default
-
-
-
-    
+    nib.save(inf_nifti, outname)  # TODO: sensible default
 
 
 if __name__ == "__main__":
