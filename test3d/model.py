97122eb905c7195a73dd9d741f81ad9f321fc772
sriemannn <steffenriemann@web.de>
2024-06-04 14:09:08 +0200

    working 3d model; fixed wrong loss

--- test3d/model.py
+++ test3d/model.py
@@ -1,23 +1,42 @@
-from glob import glob
-import numpy as np
 import os.path as op
+from glob import glob
+
+import matplotlib.pyplot as plt
 import monai.transforms as tfms
+import numpy as np
+import torch
 from monai.data import ArrayDataset, DataLoader, decollate_batch
+from monai.losses import DiceLoss
 from monai.metrics import DiceMetric
 from monai.networks.nets import UNet
-from monai.losses import DiceLoss
-import torch
 from tqdm import tqdm
 
 
 def subsetting(
     subset: str,
-    vols: list,
-    mask: list,
+    vols: list[str],
+    mask: list[str],
     validation_cases: int,
     test_cases: int,
     seed: int = 42,
-):
+) -> tuple[list[str], list[str]]:
+    """From a list of volume and mask files create the subset specified
+
+    Parameters
+    -----------
+    subset(str): Create a subset for "train", "validation", or "test"
+        purposes
+    vols(list[str]): List with paths to the image data
+    mask(list[str]): List with paths to the ground truth mask data
+    validation_cases(int): Number of Images for validation
+    test_cases(int): Number of Images for tests
+    seed(int): Seed for the randomization. Keep the same for the same
+        model, default 42
+
+    Returns
+    -----------
+    tuple[list[str],list[str]]: (image subset, mask subset)
+    """
     vols = np.asarray(vols)
     mask = np.asarray(mask)
 
@@ -36,22 +55,44 @@ def subsetting(
     return vols[sampling_array == subset], mask[sampling_array == subset]
 
 
-def create_dataset(subset: str, validation_cases: int, test_cases: int, seed: int = 42):
-    volume_suffix: str = "petra_cut_pads.nii.gz"
-    mask_suffix: str = "cylinder_plus_plug_ROI.nii.gz"
-    root_dir = "/media/MeMoSLAP_Subjects/derivatives/automated_electrode_extraction"
-    subject_pattern = op.join(
-        root_dir,
-        "sub-*",
-        "electrode_extraction",
-        "ses-*",
-        "run-*",
-    )
+def create_dataset(
+    subset: str, validation_cases: int, test_cases: int, seed: int = 42
+) -> ArrayDataset:
+    """
+    Load Dataset and transformations for MONAI
+
+    Parameters
+    -----------
+    subset(str): Create a dataset for "train", "validation", or "test"
+        purposes
+    vols(list[str]): List with paths to the image data
+    mask(list[str]): List with paths to the ground truth mask data
+    validation_cases(int): Number of Images for validation
+    test_cases(int): Number of Images for tests
+    seed(int): Seed for the randomization. Keep the same for the same
+        model, default 42
+
+    Returns
+    -----------
+    ArrayDataset: Dataset for model
+
+    Raises
+    -----------
+    ValueError: No MRI-volumes in the current path
+
+    """
+    volume_suffix: str = "*volume_*.nii.gz"
+    mask_suffix: str = "*mask_*.nii.gz"
+    root_dir = "./data"
+    subject_pattern = op.join(root_dir, "tmp")
 
     volumes = glob(op.join(subject_pattern, volume_suffix))
     volumes.sort()
     masks = glob(op.join(subject_pattern, mask_suffix))
     masks.sort()
+
+    if not volumes:
+        raise ValueError("No MRI-volumes found.")
 
     masks = [m for m in masks if op.exists(m)]
     volumes = [v for v, m in zip(volumes, masks) if op.exists(m)]
@@ -70,9 +111,19 @@ def create_dataset(subset: str, validation_cases: int, test_cases: int, seed: in
             tfms.LoadImage(image_only=True),
             tfms.ScaleIntensity(),
             tfms.EnsureChannelFirst(),
-            tfms.RandRotate(prob=1, keep_size=True),
             tfms.RandZoom(1, min_zoom=0.7, max_zoom=1.3),
-            tfms.RandAffine(1),
+            tfms.RandRotate(
+                prob=1,
+                range_x=0.5,
+                range_y=0.5,
+                range_z=0.5,
+                keep_size=True,
+            ),
+            tfms.RandAffine(
+                prob=1, rotate_range=0.5, shear_range=0.5, padding_mode="zeros"
+            ),
+            tfms.Resize((64, 64, 64)),
+            tfms.SignalFillEmpty(),
         ]
     )
 
@@ -80,188 +131,226 @@ def create_dataset(subset: str, validation_cases: int, test_cases: int, seed: in
         [
             tfms.LoadImage(image_only=True),
             tfms.EnsureChannelFirst(),
-            tfms.RandRotate(prob=0.7, keep_size=True),
             tfms.RandZoom(1, min_zoom=0.7, max_zoom=1.3),
-            tfms.RandAffine(1),
+            tfms.RandRotate(
+                prob=1,
+                range_x=0.5,
+                range_y=0.5,
+                range_z=0.5,
+                keep_size=True,
+            ),
+            tfms.RandAffine(
+                prob=1, rotate_range=0.5, shear_range=0.5, padding_mode="zeros"
+            ),
+            tfms.Resize((64, 64, 64)),
+            tfms.SignalFillEmpty(),
         ]
     )
 
     return ArrayDataset(volumes, vol_tfms, masks, mask_tfms)
 
 
-@torch.no_grad()
-def validation(net, global_step, epoch_iterator_val, dice_metric):
-    net.eval()
-    for batch in epoch_iterator_val:
-        val_inputs, val_labels = batch[0].cuda(), batch[1].cuda()
-        val_output = net(val_inputs)
-        dice_metric(y_pred=val_output, y=val_labels)
-        epoch_iterator_val.set_description(
-            "Validate (%d / %d Steps)" % (global_step, 10.0)
-        )  # noqa: B038
-    mean_dice_val = dice_metric.aggregate().item()
-    dice_metric.reset()
-    return mean_dice_val
+class Network:
 
-
-def train(
-    net,
-    loss_function,
-    global_step,
-    train_loader,
-    val_loader,
-    dice_val_best,
-    global_step_best,
-    opt,
-    dice_metric,
-    eval_num,
-    max_iterations,
-    epoch_loss_values,
-    metric_values,
-    root_dir,
-):
-    net.train()
-    epoch_loss = 0
-    step = 0
-    epoch_iterator = tqdm(
-        train_loader, desc="Training (X / X Steps) (loss=X.X)", dynamic_ncols=True
-    )
-    for step, batch in enumerate(epoch_iterator):
-        step += 1
-        x, y = batch[0].cuda(), batch[1].cuda()
-        with torch.cuda.amp.autocast():
-            logit_map = net(x)
-            loss = loss_function(logit_map, y)
-
-        epoch_loss += loss.item()
-        loss.backward()
-        opt.step()
-        epoch_iterator.set_description(  # noqa: B038
-            f"Training ({global_step} / {max_iterations} Steps) (loss={loss:2.5f})"
-        )
-
-        if (global_step % eval_num != 0) or (global_step != max_iterations):
-            global_step += 1
-            continue
-
-        epoch_iterator_val = tqdm(
-            val_loader, desc="Validate (X / X Steps) (dice=X.X)", dynamic_ncols=True
-        )
-        dice_val = validation(net, global_step, epoch_iterator_val, dice_metric)
-        epoch_loss /= step
-        epoch_loss_values.append(epoch_loss)
-        metric_values.append(dice_val)
-        if dice_val > dice_val_best:
-            dice_val_best = dice_val
-            global_step_best = global_step
-            torch.save(net.state_dict(), op.join(root_dir, "best_metric_model.pth"))
-            print(
-                "Model Was Saved! Current Best Avg. Dice: {} Current Avg. Dice: {}".format(
-                    dice_val_best, dice_val
-                )
-            )
-        else:
-            print(
-                "Model Was Not Saved! Current Best Avg. Dice: {} Current Avg. Dice: {}".format(
-                    dice_val_best, dice_val
-                )
-            )
-        global_step += 1
-    return (
+    def __init__(
+        self,
         net,
-        loss_function,
-        global_step,
-        dice_val_best,
-        global_step_best,
+        scaler,
         opt,
-        eval_num,
-        epoch_loss_values,
-        metric_values,
-    )
+        loss_function,
+        train_loader,
+        val_loader,
+        dice_metric,
+        eval_num: int,
+        max_iterations: int,
+        root_dir: str,
+    ):
+        self.net = net
+        self.scaler = scaler
+        self.opt = opt
+        self.loss_function = loss_function
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.dice_metric = dice_metric
+        self.eval_num = eval_num
+        self.max_iterations = max_iterations
+        self.epoch_loss_values = []
+        self.metric_values = []
+        self.dice_val_best = 0
+        self.global_step_best = 0
+        self.global_step = 0
+        self.root_dir = root_dir
+
+    def train(self):
+        """
+        Train-Loop for the UNet
+        """
+        self.net.train()
+        epoch_loss = 0
+        step = 0
+        epoch_iterator = tqdm(
+            self.train_loader,
+            desc="Training (X / X Steps) (loss=X.X)",
+            dynamic_ncols=True,
+        )
+        for step, batch in enumerate(epoch_iterator):
+            step += 1
+            x, y = batch[0].cuda(), batch[1].cuda()
+
+            with torch.cuda.amp.autocast():
+                logit_map = self.net(x)
+                loss = self.loss_function(logit_map, y)
+
+            with open("loss.txt", "a") as f:
+                f.write(f"{str(loss.item())}\n")
+
+            self.opt.zero_grad()
+            self.scaler.scale(loss).backward()
+            epoch_loss += loss.item()
+            self.scaler.unscale_(self.opt)
+            self.scaler.step(self.opt)
+            self.scaler.update()
+            self.opt.zero_grad()
+            epoch_iterator.set_description(
+                f"Training ({self.global_step} / {self.max_iterations} Steps) (loss={loss:2.5f})"
+            )
+
+            if (
+                (self.global_step % self.eval_num != 0)
+                and (self.global_step != self.max_iterations)
+                and (self.global_step != 1)
+            ):
+                self.global_step += 1
+                continue
+            dice_val = self.validation()
+            epoch_loss /= step
+            with open("epoch_loss.txt", "a") as f:
+                f.write(f"{str(loss.item())}\n")
+            self.epoch_loss_values.append(epoch_loss)
+            self.metric_values.append(dice_val)
+            if dice_val < self.dice_val_best:
+                print(
+                    "Model Was Not Saved! Current Best Avg. Dice: "
+                    f"{self.dice_val_best} Current Avg. Dice: {dice_val}"
+                )
+                self.global_step += 1
+                return
+
+            self.dice_val_best = dice_val
+            self.global_step_best = self.global_step
+            torch.save(
+                self.net.state_dict(), op.join(self.root_dir, "best_metric_model.pth")
+            )
+            print(
+                f"Model Was Saved! Current Best Avg. Dice: {self.dice_val_best}"
+                f" Current Avg. Dice: {dice_val}"
+            )
+            self.global_step += 1
+
+    @torch.no_grad()
+    def validation(self):
+        """
+        Validation of the UNet
+        """
+        post_pred = tfms.Compose([tfms.AsDiscrete(argmax=True, to_onehot=2)])
+        post_label = tfms.Compose([tfms.AsDiscrete(to_onehot=2)])
+        # post_pred2 = tfms.Compose([tfms.AsDiscrete(argmax=True)])
+        # sigmoid = torch.nn.Sigmoid()
+        epoch_iterator_val = tqdm(
+            self.val_loader,
+            desc="Validate (X / X Steps) (dice=X.X)",
+            dynamic_ncols=True,
+        )
+        self.net.eval()
+        for batch in epoch_iterator_val:
+            val_inputs, val_labels = batch[0].cuda(), batch[1].cuda()
+            val_output = self.net(val_inputs)
+
+            val_output_ = val_output.cpu().numpy()
+            val_labels_ = val_labels.cpu().numpy()
+
+            np.save("val_out2.npy", val_output_)
+            np.save("val_in2.npy", val_labels_)
+
+            self.dice_metric(
+                y_pred=[post_pred(i) for i in decollate_batch(val_output)],
+                y=[post_label(i) for i in decollate_batch(val_labels)],
+            )
+            epoch_iterator_val.set_description(
+                "Validate (%d / %d Steps)" % (self.global_step, 10.0)
+            )
+        mean_dice_val = self.dice_metric.aggregate().item()
+        with open("mean_dice_val.txt", "a") as f:
+            f.write(f"{str(mean_dice_val)}\n")
+        self.dice_metric.reset()
+        return mean_dice_val
 
 
-def main():
+def alt_main() -> None:
     vc, tc = 8, 8
-    train_dataset = create_dataset(subset="train", validation_cases=vc, test_cases=tc)
+    bs = 32
+    seed = 1001
+    train_dataset = create_dataset(
+        subset="train", validation_cases=vc, test_cases=tc, seed=seed
+    )
     train_loader = DataLoader(
-        train_dataset, batch_size=1, num_workers=2, pin_memory=torch.cuda.is_available()
+        train_dataset,
+        batch_size=bs,
+        num_workers=2,
+        pin_memory=torch.cuda.is_available(),
+        shuffle=True,
     )
     val_dataset = create_dataset(
-        subset="validation", validation_cases=vc, test_cases=tc
+        subset="validation", validation_cases=vc, test_cases=tc, seed=seed
     )
     val_loader = DataLoader(
-        val_dataset, batch_size=1, num_workers=2, pin_memory=torch.cuda.is_available()
+        val_dataset, batch_size=bs, num_workers=2, pin_memory=torch.cuda.is_available()
     )
-
     device = torch.device("cuda:0")
     net = UNet(
         spatial_dims=3,
         in_channels=1,
-        out_channels=1,
-        channels=(64, 128, 256, 512, 1024),
+        out_channels=2,
+        channels=(32, 64, 128, 256, 512),
         strides=(2, 2, 2, 2),
         num_res_units=2,
+        act="RELU",
     ).to(device)
 
-    loss_function = DiceLoss(sigmoid=True)
-    dice_metric = DiceMetric(reduction="mean")
-    lr = 1e-3
-    opt = torch.optim.Adam(net.parameters(), lr)
-    max_iterations = 30_000
-    eval_num = 500
-    global_step = 0
-    dice_val_best = 0.0
-    global_step_best = 0
-    epoch_loss_values = []
-    metric_values = []
-
-    root_dir = op.dirname(op.abspath(__file__))
-    while global_step < max_iterations:
-        (
-            net,
-            loss_function,
-            global_step,
-            dice_val_best,
-            global_step_best,
-            opt,
-            eval_num,
-            epoch_loss_values,
-            metric_values,
-        ) = train(
-            net,
-            loss_function,
-            global_step,
-            train_loader,
-            val_loader,
-            dice_val_best,
-            global_step_best,
-            opt,
-            dice_metric,
-            eval_num,
-            max_iterations,
-            epoch_loss_values,
-            metric_values,
-            root_dir,
-        )
-    net.load_state_dict(torch.load(op.join(root_dir, "best_metric_model.pth")))
-
-    print(
-        f"train completed, best_metric: {dice_val_best:.4f}"
-        f" at iteration: {global_step_best}"
+    network = Network(
+        net=net,
+        scaler=torch.cuda.amp.GradScaler(),
+        opt=torch.optim.Adam(net.parameters(), lr=1e-4),
+        # opt=torch.optim.SGD(
+        #    net.parameters(),
+        #    lr=5e-3,
+        #    momentum=0.2
+        # ),
+        loss_function=DiceLoss(to_onehot_y=True, softmax=True),
+        train_loader=train_loader,
+        val_loader=val_loader,
+        dice_metric=DiceMetric(reduction="mean", include_background=False),
+        eval_num=500,
+        max_iterations=30_000,
+        root_dir=op.dirname(op.abspath(__file__)),
     )
 
-    plt.figure("train", (12, 6))
-    plt.subplot(1, 2, 1)
-    plt.title("Average Loss per Iteration")
-    x = [eval_num * (i + 1) for i in range(len(epoch_loss_values))]
-    plt.xlabel("Iteration")
-    plt.plot(x, epoch_loss_values)
-    plt.subplot(1, 2, 2)
-    plt.title("Validation Mean Dice")
-    x = [eval_num * (i + 1) for i in range(len(metric_values))]
-    plt.xlable("Iteration")
-    plt.plot(x, y)
-    plt.show()
+    while network.global_step < network.max_iterations:
+        network.train()
 
-if __name__ == "__main__":
-    main()
+    print(
+        f"train completed, best_metric: {network.dice_val_best:.4f}"
+        f" at iteration: {network.global_step_best}"
+    )
+
+    _, axes = plt.subplots(nrows=1, ncols=2)
+
+    for data, ax in zip(
+        [network.epoch_loss_values, network.metric_values],
+        axes.flatten(),
+    ):
+        x = [network.eval_num * (i + 1) for i in range(len(data))]
+        ax.plot(x, data)
+        plt.xlabel("Iteration")
+
+    plt.show()
