a9e59d776def14c460627b1ee37394334c522b29
sriemannn <steffenriemann@web.de>
2024-10-11 09:57:06 +0200

    added documentation to the CONSTANTS

--- test3d/docker/fileconfig.py
+++ test3d/docker/fileconfig.py
@@ -1,3 +1,27 @@
+"""
+Configuration file with constants for `docker_model.py`
+
+CONSTANTS:
+    SUBJECT_PATTERN: str
+        Pattern for subject directories. Just add an asterisk (*)
+        for every changing part of the path.
+    VOLUME_SUFFIX: str
+        How the volume files are named. Could also include a path
+        or asterisks to indicate a pattern, but should only
+        lead to one file for every subject, session, run combination.
+    MASK_SUFFIX: str
+        How the mask files are named. Could also include a path
+        or asterisks to indicate a pattern, but should only
+        lead to one file for every subject, session, run combination.
+    INPUT_DIR: str
+        Path to the input directory. This is how the virtual volume
+        should be named in the Docker/Singularity container.
+    OUTPUT_DIR: str
+        Path to the output directory. This is how the virtual output
+        directory should be named in the Docker/Singularity container.
+"""
+
+
 SUBJECT_PATTERN = "sub-*/electrode_extraction/ses-*/run-*"
 VOLUME_SUFFIX = "petra_.nii.gz"
 MASK_SUFFIX = "petra_masked.nii.gz"
